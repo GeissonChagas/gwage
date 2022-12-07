a80d3a6b6783667eaf7c4062b60de72ae069eefd
GeissonChagas <geisson.excel@gmail.com>
2022-12-06 23:15:34 -0300

    adicionando modificacoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,45 @@
-const form = document.getElementById("formulario-produtos");    
+// Adiciona produtos na tabela:
 
-form.addEventListener("submit", (evento) => {
+const adicionaItens = document.getElementById("formulario-produtos");    
+
+adicionaItens.addEventListener("submit", function(evento){
     evento.preventDefault()
-    console.log("funcionou!")
+
+    let form = document.getElementById("formulario-produtos")
+
+    let produto = form.produto.value;
+    let preco   = form.preco.value;
+
+
+    let produtoTr = document.createElement("tr")
+    produtoTr.setAttribute('class', 'produto__tr')
+
+    let itemTd = document.createElement("td")
+    itemTd.setAttribute('class', 'produto__td')
+
+    let produtoTd = document.createElement("td")
+    produtoTd.setAttribute('class', 'produto__td')
+
+    let precoTd = document.createElement("td")
+    precoTd.setAttribute('class', 'produto__td')
+
+    let excluirTd = document.createElement("td")
+    excluirTd.setAttribute('class', 'produto__td')
+
+    itemTd.textContent = "01"
+    produtoTd.textContent = produto;
+    precoTd.textContent = preco;
+    excluirTd.textContent = document.getElementById('excluir');
+
+    produtoTr.appendChild(itemTd);
+    produtoTr.appendChild(produtoTd);
+ 
+
+
+    let tabela = document.getElementById('tbody')
+
+
+
+  
+
 })
