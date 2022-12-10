c2e61cbb2757ba579e5d7ab00c81c5de7cbdb049
GeissonChagas <geisson.excel@gmail.com>
2022-12-09 22:22:46 -0300

    local storage

--- assets/js/excluir.js
+++ assets/js/excluir.js
@@ -1,9 +1,9 @@
-const excluir = document.querySelectorAll(".produto__excluir");
+
 const tabela = document.querySelector('#excluir');
 
 tabela.addEventListener("click", function(evento){
     
-     let alvo = evento.target;
+    let alvo = evento.target;
     let alvoPai = alvo.parentNode;
 
     alvoPai.remove();
