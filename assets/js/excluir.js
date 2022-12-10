a8f5a3631a8cb40b3fd7858912baa1ded9443a3a
Paulo Santana <paulosantana.sa@gmail.com>
2022-12-10 01:10:06 -0300

    resolvendo conflito com a excluir.js

--- assets/js/excluir.js
+++ assets/js/excluir.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-
-const tabela = document.querySelector('#excluir');
-
-tabela.addEventListener("click", function(evento){
-    
-    let alvo = evento.target;
-    let alvoPai = alvo.parentNode;
-
-    alvoPai.remove();
-=======
 const tabela = document.querySelector('#tbody');
 
 tabela.addEventListener("click", function(evento){
->>>>>>> paulo
 
     if(evento.target.className == 'produto__excluir') {
         evento.target.parentNode.remove();
